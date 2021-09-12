222b6866c84f6b03612474c7c4df6205884a78f3
aethelind <aethelind@gmail.com>
2021-09-12 13:14:49 -0400

    Validate image size and type

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,8 @@ class Image < ApplicationRecord
   has_one_attached :image
   validate :acceptable_image
 
+  # Validations from:
+  # https://pragmaticstudio.com/tutorials/using-active-storage-in-rails
   def acceptable_image
     # Ensure attachment exists
     return unless image.attached?
@@ -11,6 +13,10 @@ class Image < ApplicationRecord
     unless image.byte_size <= 3.megabyte
       errors.add(:image, "is larger than 3 MB.")
     end
-
+    
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a JPEG or PNG")
+    end
   end
 end
