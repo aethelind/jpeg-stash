597469ca34f4f65e57b221ac14feda7311560a17
aethelind <aethelind@gmail.com>
2021-09-16 19:30:27 -0400

    Add headless chrome config for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,4 +32,12 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:each) do
+    if  ENV['HEADLESS'] == 'true'
+      Capybara.current_driver = :selenium_chrome_headless
+    else
+      Capybara.current_driver = :selenium_chrome
+    end
+  end
 end
