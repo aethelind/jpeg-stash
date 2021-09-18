2090eae12cf4f04a30eda259427ffe46c24f4b49
aethelind <aethelind@gmail.com>
2021-09-18 13:27:30 -0400

    Fix image routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  resources :images, only: [:new, :show, :create, :destroy, :edit]
+  resources :images, only: [:new, :show, :create, :destroy, :edit, :update]
   get 'home/index'
   root 'home#index'
   
   resources :users, only: [:new, :show, :create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
-
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/destroy'
   
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
